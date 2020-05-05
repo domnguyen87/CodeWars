2fcaea8b2f1a00fe84fcb6ca68fee52d80bd5816
Dominique Nguyen <domnguyen@gmail.com>
2020-05-04 17:51:17 -0700

    mew question added

---
+++ 24.subtract.cs
@@ -0,0 +1,28 @@
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+// Example 1:
+// Input: n = 234
+// Output: 15 
+// Explanation: 
+// Product of digits = 2 * 3 * 4 = 24 
+// Sum of digits = 2 + 3 + 4 = 9 
+// Result = 24 - 9 = 15
+
+public class Solution
+{
+    public int SubtractProductAndSum(int n)
+    {
+        int prod = 1;
+        int sum = 0;
+
+        while( n!= 0)
+        {
+            int digit = n % 10;
+            prod *= digit;
+            sum += digit;
+
+            n = n / 10;
+        }
+
+        return prod - sum;
+    }
+}
